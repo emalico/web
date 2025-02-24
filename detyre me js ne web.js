64919aca99daee297e69585e9be59d83693d1c54
emalico <emalicohii@gmail.com>
2025-02-24 10:08:28 +0100

    Add files via upload

---
+++ detyre me js ne web.js
@@ -0,0 +1,10 @@
+
+class student{
+    constructor(fullName, datelindja, universiteti){
+       this.fullName=fullName,
+        this.datelindja=datelindja,
+        this.universiteti=universiteti
+//mos vendos presje te e fundit
+    }
+}
+const Studenti1=new student("ema",12/2/2006,"Hermann gmeiner");
